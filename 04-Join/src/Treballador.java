884a768396f915363f35e8e8b0bb77ffbfce6519
Akasha Karam <akashakaram3@gmail.com>
2026-01-21 13:13:44 +0100

    Acabat desde casa

--- 04-Join/src/Treballador.java
+++ 04-Join/src/Treballador.java
@@ -8,46 +8,37 @@ public class Treballador extends Thread {
     private float cobrat;
     private Random rnd;
 
-    public Treballador(String nom, float sou_anual_brut, int edat_inici, int edat_fi) {
-        super(nom); 
-        this.sou_anual_brut = sou_anual_brut;
-        this.edat_inici_treball = edat_inici;
-        this.edat_fi_treball = edat_fi;
+    public Treballador(String nom, float sou, int inici, int fi) {
+        super(nom); // Establece el nombre del Thread
+        this.sou_anual_brut = sou;
+        this.edat_inici_treball = inici;
+        this.edat_fi_treball = fi;
         this.edat_actual = 0;
         this.cobrat = 0.0f;
         this.rnd = new Random();
     }
 
     public void cobra() {
-        this.cobrat += (this.sou_anual_brut / 12);
+        this.cobrat += (sou_anual_brut / 12.0f);
     }
 
     public void pagaImpostos() {
-        float mensual = this.sou_anual_brut / 12;
-        this.cobrat -= (mensual * 0.24f);
+        float impost = (sou_anual_brut / 12.0f) * 0.24f;
+        this.cobrat -= impost;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i <= edat_fi_treball; i++) {
-            this.edat_actual = i;
-
-            if (edat_actual >= edat_inici_treball && edat_actual < edat_fi_treball) {
-                for (int mes = 1; mes <= 12; mes++) {
+        for (edat_actual = 0; edat_actual < edat_fi_treball; edat_actual++) {
+            if (edat_actual >= edat_inici_treball) {
+                for (int mes = 0; mes < 12; mes++) {
                     cobra();
                     pagaImpostos();
                 }
             }
         }
-        System.out.printf("%s -> edat: %d / total: %.2f%n", 
-                this.getName(), this.edat_actual, this.cobrat);
     }
 
-    public float getCobrat() {
-        return cobrat;
-    }
-
-    public int getEdat() {
-        return edat_actual;
-    }
+    public float getCobrat() { return cobrat; }
+    public int getEdat() { return edat_actual; }
 }
